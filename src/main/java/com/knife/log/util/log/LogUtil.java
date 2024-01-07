d2e1e9a8e31fce153b6f9c0e313bdf23456fb9b2
knfeedge <2716552863@qq.com>
2024-01-07 17:16:24 +0800

    初版项目

---
+++ src/main/java/com/knife/log/util/log/LogUtil.java
@@ -0,0 +1,33 @@
+package com.knife.log.util.log;
+
+import com.knife.log.util.log.bo.LogBO;
+import com.knife.log.util.log.inner.bo.LogInnerBO;
+import com.knife.log.util.log.inner.util.LogInnerUtil;
+import org.springframework.beans.BeanUtils;
+
+public class LogUtil {
+    public static void info(LogBO logBO) {
+        convertAndRecord(logBO, "INFO");
+    }
+
+    public static void warning(LogBO logBO) {
+        convertAndRecord(logBO, "WARNING");
+    }
+
+    public static void error(LogBO logBO) {
+        convertAndRecord(logBO, "ERROR");
+    }
+
+    /**
+     * 转换实体类并记录日志
+     */
+    private static void convertAndRecord(LogBO logBO, String level) {
+        LogInnerBO logInnerBO = new LogInnerBO();
+        BeanUtils.copyProperties(logBO, logInnerBO);
+        logInnerBO.setLogTime(level);
+        logInnerBO.setType("手动打印");
+        logInnerBO.setLevel(level);
+
+        LogInnerUtil.record(logInnerBO);
+    }
+}
