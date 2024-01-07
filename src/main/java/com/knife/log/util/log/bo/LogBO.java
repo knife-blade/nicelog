08606e91a9e42b2bc43043a8f9af7a6d1534870f
knfeedge <2716552863@qq.com>
2024-01-07 17:16:24 +0800

    初版项目

---
+++ src/main/java/com/knife/log/util/log/bo/LogBO.java
@@ -0,0 +1,70 @@
+package com.knife.log.util.log.bo;
+
+import com.knife.example.common.util.log.LogUtil;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogBO {
+    /**
+     * 参数
+     */
+    private String param;
+
+    /**
+     * 返回值
+     */
+    private String returnValue;
+
+    /**
+     * 自定义信息
+     */
+    private String customInfo;
+
+    /**
+     * 异常简要信息
+     */
+    private String exceptionInfo;
+
+    /**
+     * 异常详细信息
+     */
+    private String exceptionDetail;
+
+    /**
+     * 请求头
+     */
+    private String header;
+
+    /**
+     * 其他1
+     */
+    private String other1;
+
+    /**
+     * 其他2
+     */
+    private String other2;
+
+    /**
+     * 其他3
+     */
+    private String other3;
+
+    public void info() {
+        LogUtil.info(this);
+    }
+
+    public void warning() {
+        LogUtil.warning(this);
+    }
+
+    public void error() {
+        LogUtil.error(this);
+    }
+}
