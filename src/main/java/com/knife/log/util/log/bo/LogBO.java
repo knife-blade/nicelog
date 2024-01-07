a78fc6ae3bc9cdc3671c67f1c90b598ef13fa9c2
knfeedge <2716552863@qq.com>
2024-01-07 21:30:27 +0800

    转移实现类的位置

--- src/main/java/com/knife/log/util/log/bo/LogBO.java
+++ src/main/java/com/knife/log/util/log/bo/LogBO.java
@@ -1,6 +1,6 @@
 package com.knife.log.util.log.bo;
 
-import com.knife.example.common.util.log.LogUtil;
+import com.knife.log.util.log.LogUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
