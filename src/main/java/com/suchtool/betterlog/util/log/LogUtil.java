652c1e5a3f2a3f7e1716534284e2dcfd45d27439
knifeout <2716552863@qq.com>
2024-01-26 15:44:19 +0800

    解决报错问题

--- src/main/java/com/suchtool/betterlog/util/log/LogUtil.java
+++ src/main/java/com/suchtool/betterlog/util/log/LogUtil.java
@@ -1,30 +1,37 @@
 package com.suchtool.betterlog.util.log;
 
+import com.suchtool.betterlog.constant.LogLevelEnum;
 import com.suchtool.betterlog.util.log.bo.LogBO;
 import com.suchtool.betterlog.util.log.inner.bo.LogInnerBO;
 import com.suchtool.betterlog.util.log.inner.util.LogInnerUtil;
+import com.suchtool.betterutil.constant.DateTimeFormatConstant;
+import com.suchtool.betterutil.util.DateTimeUtil;
 import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDateTime;
 
 public class LogUtil {
     public static void info(LogBO logBO) {
-        convertAndRecord(logBO, "INFO");
+        convertAndRecord(logBO, LogLevelEnum.INFO);
     }
 
     public static void warning(LogBO logBO) {
-        convertAndRecord(logBO, "WARNING");
+        convertAndRecord(logBO, LogLevelEnum.WARNING);
     }
 
     public static void error(LogBO logBO) {
-        convertAndRecord(logBO, "ERROR");
+        convertAndRecord(logBO, LogLevelEnum.ERROR);
     }
 
     /**
      * 转换实体类并记录日志
      */
-    private static void convertAndRecord(LogBO logBO, String level) {
+    private static void convertAndRecord(LogBO logBO, LogLevelEnum level) {
         LogInnerBO logInnerBO = new LogInnerBO();
         BeanUtils.copyProperties(logBO, logInnerBO);
-        logInnerBO.setLogTime(level);
+        logInnerBO.setLogTime(DateTimeUtil.format(LocalDateTime.now(),
+                DateTimeFormatConstant.DATE_TIME_FORMAT_NORMAL));
+
         logInnerBO.setTypeDetail("手动打印");
         logInnerBO.setLevel(level);
 
