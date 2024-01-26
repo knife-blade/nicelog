2b4a88947e9a0bf1c7a32363e689160e8ad98de7
knifeout <2716552863@qq.com>
2024-01-26 16:10:41 +0800

    支持XxlJob

--- src/main/java/com/suchtool/betterlog/aspect/LogAspectProcessor.java
+++ src/main/java/com/suchtool/betterlog/aspect/LogAspectProcessor.java
@@ -1,10 +1,12 @@
 package com.suchtool.betterlog.aspect;
 
+import java.lang.reflect.Method;
+
 public interface LogAspectProcessor extends LogParamProvider{
     /**
      * 判断是否需要处理
      */
-    boolean requireProcess();
+    boolean requireProcess(Method method);
 
     /**
      * 正常返回或者抛异常的处理
