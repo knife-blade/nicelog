f6dcba4a0e266386fb5ae7fce36c8ecf7d0bafef
knifeout <2716552863@qq.com>
2024-01-26 17:02:22 +0800

    支持自定义处理逻辑

---
+++ src/main/java/com/suchtool/betterlog/process/BetterLogProcess.java
@@ -0,0 +1,7 @@
+package com.suchtool.betterlog.process;
+
+import com.suchtool.betterlog.util.log.inner.bo.LogInnerBO;
+
+public interface BetterLogProcess {
+    void process(LogInnerBO logInnerBO);
+}
