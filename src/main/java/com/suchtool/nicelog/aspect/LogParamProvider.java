d227565708c6eb096bcb170a2e9bf3b011f7dd85
knifeout <2716552863@qq.com>
2024-01-30 18:01:44 +0800

    使用util的方法工具类

--- src/main/java/com/suchtool/nicelog/aspect/LogParamProvider.java
+++ src/main/java/com/suchtool/nicelog/aspect/LogParamProvider.java
@@ -1,15 +1,9 @@
 package com.suchtool.nicelog.aspect;
 
 import com.suchtool.nicelog.constant.AspectTypeEnum;
-import com.suchtool.nicelog.util.MethodUtil;
-import org.springframework.context.expression.MethodBasedEvaluationContext;
-import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
+import com.suchtool.niceutil.util.MethodUtil;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 日志参数提供者
@@ -36,24 +30,6 @@ public interface LogParamProvider {
     }
 
     default Object provideParam(Object[] args, Method method) {
-        return MethodUtil.parseParam(args, method);
-
-        // if (args == null
-        //     || args.length == 0) {
-        //     return null;
-        // }
-        //
-        // if (args.length == 1) {
-        //     return args[0];
-        // }
-        //
-        // Map<String, Object> map = new HashMap<>();
-        // int i = 1;
-        // for (Object arg : args) {
-        //     String key = "arg" + i;
-        //     map.put(key, arg);
-        //     i++;
-        // }
-        // return map;
+        return MethodUtil.parseParam(method, args);
     }
 }
