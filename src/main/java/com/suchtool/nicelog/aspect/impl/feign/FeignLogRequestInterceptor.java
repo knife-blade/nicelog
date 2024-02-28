2b63d48d77be118fbb9de0827c7a79382abb7dff
knifeout <2716552863@qq.com>
2024-02-28 15:55:22 +0800

    解决traceID问题

--- src/main/java/com/suchtool/nicelog/aspect/impl/feign/FeignLogRequestInterceptor.java
+++ src/main/java/com/suchtool/nicelog/aspect/impl/feign/FeignLogRequestInterceptor.java
@@ -1,13 +1,18 @@
 package com.suchtool.nicelog.aspect.impl.feign;
 
-import com.suchtool.nicelog.constant.TraceIdConstant;
+import com.suchtool.nicelog.property.NiceLogProperty;
 import com.suchtool.nicelog.util.NiceLogTraceIdUtil;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FeignLogRequestInterceptor implements RequestInterceptor {
+    @Autowired
+    private NiceLogProperty niceLogProperty;
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        requestTemplate.header(TraceIdConstant.TRACE_ID_HEADER, NiceLogTraceIdUtil.readTraceId());
+        requestTemplate.header(
+                niceLogProperty.getFeignTraceIdHeader(),
+                NiceLogTraceIdUtil.readTraceId());
     }
 }
